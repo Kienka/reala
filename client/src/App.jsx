8cc736c349b6990896c73595f62f774332f66b73
Kienka Kio <kienkakio@hotmail.com>
2025-08-03 16:36:20 +0300

    Added routes header and nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Kienka</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path="/signin" element={<SignIn  />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+            <Link to="/">
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>Reala</span>
+                <span className='text-slate-700'> - Your Real Estate App</span>
+            </h1>
+            </Link>
+             <form className='bg-slate-100 p-3 rounded-md flex items-center'>
+            <input
+                type="text"
+                placeholder="Search..."
+                className="bg-transparent focus:outline-none w-24 sm:w-64"
+            />
+            <FaSearch className='text-slate-600 cursor-pointer' />
+        </form>
+        <ul className='flex space-x-4'>
+           <Link to="/"><li className='hidden sm:inline text-slate-700 hover:text-slate-900'>Home</li></Link>
+            <Link to="/about"><li className=' hidden sm:inline text-slate-700 hover:text-slate-900'>About</li></Link>
+            <Link to="/signin"><li className='sm:inline text-slate-700 hover:text-slate-900'>Sign In</li></Link>
+            </ul>
+            
+        </div>
+       
+    </header>
+  )
+}
+
+export default Header
